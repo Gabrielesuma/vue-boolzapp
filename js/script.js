dc0d5d06a0f16c0afbd4e1f3f31943e789c193af
Gabrielesuma <gabrielesuma139@gmail.com>
2024-04-04 16:06:04 +0200

    milestone 3

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 import { contacts } from "./data.js";
 
 const {createApp} = Vue;
+const dt = luxon.DateTime;
 
 createApp({
     data(){
         return{
             contacts: contacts,
             activeContact: 1,
+            messageText: ''
         }
     },
     methods:{
         changeContact(id){
             this.activeContact = id;
+        },
+        createMessage(msg, status){
+            const newMessage = {
+                date: dt.now().setLocale('it').toFormat('dd/MM/yyyy HH:mm:ss'),
+                message: msg,
+                status: status
+            }
+            return newMessage;
+        },
+        sendMessage(){
+            const newMessage = this.createMessage(this.messageText, 'sent');
+            this.activeContacts.messages.push(newMessage);
+            this.messageText = '';
+            setTimeout(()=> {
+                const newMessage = this.createMessage('ok', 'received');
+                this.activeContacts.messages.push(newMessage);
+            }, 1000);
         }
     },
     computed:{
